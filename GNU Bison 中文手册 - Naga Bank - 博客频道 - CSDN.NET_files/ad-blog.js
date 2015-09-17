4cae996ddc2e3a93c0165586ba210830bee625e9
haihua.liu <haihua.liu@tcl.com>
2015-09-17 15:38:28 +0800

    add bison chinese man html file and website data

---
+++ GNU Bison 中文手册 - Naga Bank - 博客频道 - CSDN.NET_files/ad-blog.js
@@ -0,0 +1,39 @@
+var ad_type = 'ms1459';
+var ad_sc = '<script type="text/javascript" '
+    + 'src="http://ad.csdn.net/scripts/ad-' + ad_type + '.js"'
+    + '></'
+    + 'script>';
+document.writeln(ad_sc);
+
+ad_type = 'ms1462_2';
+ad_sc = '<script type="text/javascript" '
+    + 'src="http://ad.csdn.net/scripts/ad-' + ad_type + '.js"'
+    + '></'
+    + 'script>';
+
+document.writeln(ad_sc);
+/*
+ad_type = 'ms1457_3';
+ad_sc = '<script type="text/javascript" '
+    + 'src="http://ad.csdn.net/scripts/ad-' + ad_type + '.js"'
+    + '></'
+    + 'script>';
+
+document.writeln(ad_sc);
+
+ad_type = 'ms1446_4';
+ad_sc = '<script type="text/javascript" '
+    + 'src="http://ad.csdn.net/scripts/ad-' + ad_type + '.js"'
+    + '></'
+    + 'script>';
+
+document.writeln(ad_sc);
+
+ad_type = 'ms1324_5';
+ad_sc = '<script type="text/javascript" '
+    + 'src="http://ad.csdn.net/scripts/ad-' + ad_type + '.js"'
+    + '></'
+    + 'script>';
+
+document.writeln(ad_sc);
+*/
